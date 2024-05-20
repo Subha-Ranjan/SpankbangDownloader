1f187335cd3075f1fcf8ceace7b500b96fc518aa
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-05-21 01:22:47 +0530

    deployment fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const spankbang = require("./spankbang")
 app.get("/", async(req,res)=>{
     const url = "https://spankbang.com/7slau/video/new+sensations+my+girlfriend+calls+me+while+she+s+fucking+bbc+alyx+star";
     const details = await spankbang.videos.details({url});console.log(details)
-    res.send("This is Base URL of server");
+    res.send(["This is Base URL of server", details]);
 })
 app.listen(8000,()=>{
     console.log("Server started at 8000"); 
