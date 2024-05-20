aefcb42c6eeb8191abedfff7d47e057e7eb8083d
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-05-21 00:49:45 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const app = require('express')()
-
+const express = require('express')
+const app = express()
 const spankbang = require("./spankbang")
-
-// async function callApi(){
-//     const url = "https://spankbang.com/7slau/video/new+sensations+my+girlfriend+calls+me+while+she+s+fucking+bbc+alyx+star";
-//     const details = await spankbang.videos.details({url});
-//     console.log(details)
-// }
 
 app.get("/", async(req,res)=>{
     const url = "https://spankbang.com/7slau/video/new+sensations+my+girlfriend+calls+me+while+she+s+fucking+bbc+alyx+star";
     const details = await spankbang.videos.details({url});
-    res.send(details);
+    res.send("This is Base URL of server");
 })
 app.listen(8000,()=>{
     console.log("Server started at 8000"); 
