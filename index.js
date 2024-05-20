d2cc68b4c7fee54ed75264665fef8496c950e6ba
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-05-21 00:54:19 +0530

    trying to  fix  url issues again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const spankbang = require("./spankbang")
 
 app.get("/", async(req,res)=>{
     const url = "https://spankbang.com/7slau/video/new+sensations+my+girlfriend+calls+me+while+she+s+fucking+bbc+alyx+star";
-    const details = await spankbang.videos.details({url});
+    const details = await spankbang.videos.details({url});console.log(details)
     res.send("This is Base URL of server");
 })
 app.listen(8000,()=>{
